4e510f6144614e8a8730078ebf2b15b684f64ca6
vf88 <valentin@aseno.de>
2023-07-10 15:26:00 +0200

    change user

--- app/blog-senior/page.jsx
+++ app/blog-senior/page.jsx
@@ -21,7 +21,7 @@ const BlogSeniorPage = () => {
                       <div className="mr-4">
                         <div className="relative h-10 w-10 overflow-hidden rounded-full">
                           <Image
-                            src="/images/blog/author-02.png"
+                            src="/images/blog/author-03.png"
                             alt="author"
                             fill
                           />
